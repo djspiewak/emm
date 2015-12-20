0707c3e77bcfd76c5911db277b0b87b8d729900f
Daniel Spiewak <djspiewak@gmail.com>
2015-12-20 15:13:15 -0500

    Added some non-traversable specs

--- src/test/scala/emm/EmmSpecs.scala
+++ src/test/scala/emm/EmmSpecs.scala
@@ -2,6 +2,7 @@ package emm
 
 import org.specs2.mutable._
 
+import scalaz.concurrent.Task
 import scalaz.std.list._
 import scalaz.std.option._
 
@@ -33,6 +34,33 @@ object EmmSpecs extends Specification {
       } yield v2
 
       e mustEqual Emm[E, Int](List(None, Some(2), None, Some(4)))
+    }
+  }
+
+  "non-traversable effect composition" should {
+    "allow mapping in either direction" in {
+      val opt: Option[Int] = Some(42)
+
+      opt.liftM[Task |: Option |: CCNil] map (2 *)
+      opt.liftM[Option |: Task |: CCNil] map (2 *)
+
+      ok
+    }
+
+    "allow binding where the non-traversable effect is outermost" in {
+      type E = Task |: Option |: CCNil
+      val opt: Option[Int] = Some(42)
+
+      var sink = 0
+
+      val e = for {
+        i <- opt.liftM[E]
+        _ <- (Task delay { sink += i }).liftM[E]
+      } yield ()
+
+      e.run.run
+
+      sink mustEqual 42
     }
   }
 }
