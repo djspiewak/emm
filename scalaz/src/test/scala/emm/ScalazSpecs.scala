d1071b9dba69ce60a58bcc4bca05f5e9db4f6082
Daniel Spiewak <djspiewak@gmail.com>
2016-01-24 16:56:12 -0600

    Added specs for scalaz kleisli stuff

--- scalaz/src/test/scala/emm/ScalazSpecs.scala
+++ scalaz/src/test/scala/emm/ScalazSpecs.scala
@@ -8,7 +8,7 @@ import scalaz._
 import scalaz.std.option._
 import scalaz.std.list._
 
-object StateSpecs extends Specification {
+object ScalazSpecs extends Specification {
 
   "state stacks" should {
     "implement bind" in {
@@ -22,6 +22,22 @@ object StateSpecs extends Specification {
          type E = List |: StateT[?[_], String, ?] -|: Option |: Base
 
          (42.pointM[E] flatMap { _ => "foo".pointM[E] }).run.eval("blah") mustEqual List(Some("foo"))
+      }
+    }
+  }
+
+  "kleisli stacks" should {
+    "implement bind" in {
+      "inner" >> {
+        type E = Option |: Kleisli[?[_], String, ?] -|: Base
+
+         (42.pointM[E] flatMap { _ => "foo".pointM[E] }).run.run("blah") must beSome("foo")
+       }
+
+       "mid" >> {
+         type E = List |: Kleisli[?[_], String, ?] -|: Option |: Base
+
+         (42.pointM[E] flatMap { _ => "foo".pointM[E] }).run.run("blah") mustEqual List(Some("foo"))
       }
     }
   }
